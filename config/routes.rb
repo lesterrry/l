6c2a16252123fbcea6d6d6afabcb95a6dfd6fa1b
Aydar <me@aydar.media>
2024-12-01 01:42:35 +0300

    migrated from template

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  default_url_options protocol: :https
 
-  root 'pages#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
-  get '/profile', to: 'pages#profile'
-  get '/search', to: 'pages#search'
-  get '/featured', to: 'pages#featured'
-  get '/lifetime', to: 'pages#lifetime'
+  devise_scope :user do
+    post 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
+    post 'sign_in/new_challenge', to: 'users/sessions#new_challenge', as: :new_user_session_challenge
+
+    post 'reauthenticate/new_challenge', to: 'users/reauthentication#new_challenge', as: :new_user_reauthentication_challenge
+    post 'reauthenticate', to: 'users/reauthentication#reauthenticate', as: :user_reauthentication
+
+    namespace :users do
+      resources :passkeys, only: [:index, :create, :destroy] do
+        collection do
+          post :new_create_challenge
+        end
+
+        member do
+          post :new_destroy_challenge
+        end
+      end
+    end
+  end
+
+  # Defines the root path route ("/")
+  root "root#index"
 end
